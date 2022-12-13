fb278afb7fcac86877ed923113e85d50bc6dbc51
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2022-12-13 15:15:33 +0100

    stampa + lunghezza del paragrafo

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+    //Variabile del testo
+    $txt = "PHP è un linguaggio di scripting open source generico, usato per lo più nello sviluppo web. PHP è un acronimo ricorsivo: significa che l'acronimo stesso è contenuto come prima parola nello scioglimento della sigla - infatti PHP sta per PHP: Hypertext Preprocessor.";
+    //Parola da censurare
+    $censored = "PHP"
 
 ?>
 
@@ -11,8 +15,11 @@
     <title>BadWords</title>
 </head>
 <body>
+    <!-- Stampa il testo -->
+    <p> <?php echo $txt ?> </p>
 
-<p></p>
+    <!-- Stampa la lunghezza del paragrafo -->
+    <p> Il testo contiene <?php echo strlen($txt) ?> caratteri. </p>
     
 </body>
 </html>
