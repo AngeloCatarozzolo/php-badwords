736453aca144fd03fd58560a80173e783bab01bd
AngeloCatarozzolo <angelocatarozzolo@gmail.com>
2022-12-13 15:26:44 +0100

    fine

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     //Variabile del testo
     $txt = "PHP è un linguaggio di scripting open source generico, usato per lo più nello sviluppo web. PHP è un acronimo ricorsivo: significa che l'acronimo stesso è contenuto come prima parola nello scioglimento della sigla - infatti PHP sta per PHP: Hypertext Preprocessor.";
     //Parola da censurare
-    $censored = "PHP"
-
+    $censored = "PHP";
+    $censurato = str_replace($censored,"***", $txt );
 ?>
 
 <!DOCTYPE html>
@@ -20,6 +20,12 @@
 
     <!-- Stampa la lunghezza del paragrafo -->
     <p> Il testo contiene <?php echo strlen($txt) ?> caratteri. </p>
+
+    <!-- Stampa il testo censurato -->
+    <p> <?php echo $censurato ?> </p>
+
+    <!-- Stampa la lunghezza del paragrafo censurato -->
+    <p> Il testo censurato contiene <?php echo strlen($censurato) ?> caratteri. </p>
     
 </body>
 </html>
